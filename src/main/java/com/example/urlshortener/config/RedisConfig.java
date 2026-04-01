fdf08feca1efce6f8e9aead400686e2f80011558
Shiva <work.shivapranav@gmail.com>
2026-04-01 21:56:14 +0530

    fix: RedisConfig.java

--- src/main/java/com/example/urlshortener/config/RedisConfig.java
+++ src/main/java/com/example/urlshortener/config/RedisConfig.java
@@ -1,8 +1,12 @@
 package com.example.urlshortener.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -17,5 +21,22 @@ public class RedisConfig {
         template.setValueSerializer(new StringRedisSerializer());
 
         return template;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port,
+            @Value("${spring.redis.password}") String password
+    ) {
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration(host, port);
+
+        config.setPassword(RedisPassword.of(password));
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
+        factory.setUseSsl(true);
+
+        return factory;
     }
 }
